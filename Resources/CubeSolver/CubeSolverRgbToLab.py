ab3fffba830d66438f8308479d887ee7687e2cc3
Xavier Beurois <xbeurois@djazz-univ.net>
2020-05-06 01:50:04 +0200

    Fix missing commas

--- Resources/CubeSolver/CubeSolverRgbToLab.py
+++ Resources/CubeSolver/CubeSolverRgbToLab.py
@@ -6,19 +6,19 @@ class CubeSolverRgbToLab(APIResource):
     def get(self, Red, Green, Blue):
         if Red < 0 or Red > 255:
             return Response(
-                'Red integer should be greater or equal to 0 and lesser or equal to 255'
+                'Red integer should be greater or equal to 0 and lesser or equal to 255',
                 mimetype = 'application/json',
                 status = 500
             )
         elif Green < 0 or Green > 255:
             return Response(
-                'Green integer should be greater or equal to 0 and lesser or equal to 255'
+                'Green integer should be greater or equal to 0 and lesser or equal to 255',
                 mimetype = 'application/json',
                 status = 500
             )
         elif Blue < 0 or Blue > 255:
             return Response(
-                'Blue integer should be greater or equal to 0 and lesser or equal to 255'
+                'Blue integer should be greater or equal to 0 and lesser or equal to 255',
                 mimetype = 'application/json',
                 status = 500
             )
