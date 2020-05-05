87050c8667774c914c94aed6548a0790c21e8665
Xavier Beurois <xbeurois@djazz-univ.net>
2020-05-06 01:54:30 +0200

    Use make_response from Flask

--- Resources/CubeSolver/CubeSolverRgbToLab.py
+++ Resources/CubeSolver/CubeSolverRgbToLab.py
@@ -1,34 +1,36 @@
 from flask import jsonify as JsonResponse
-from flask import Response
+from flask import make_response as MakeResponse
 from flask_restful import Resource as APIResource
 
 class CubeSolverRgbToLab(APIResource):
     def get(self, Red, Green, Blue):
         if Red < 0 or Red > 255:
-            return Response(
-                'Red integer should be greater or equal to 0 and lesser or equal to 255',
-                mimetype = 'application/json',
-                status = 500
+            return MakeResponse(
+                JsonResponse({
+                    'Message': 'Red integer should be greater or equal to 0 and lesser or equal to 255'
+                }),
+                500
             )
         elif Green < 0 or Green > 255:
-            return Response(
-                'Green integer should be greater or equal to 0 and lesser or equal to 255',
-                mimetype = 'application/json',
-                status = 500
+            return MakeResponse(
+                JsonResponse({
+                    'Message': 'Green integer should be greater or equal to 0 and lesser or equal to 255'
+                }),
+                500
             )
         elif Blue < 0 or Blue > 255:
-            return Response(
-                'Blue integer should be greater or equal to 0 and lesser or equal to 255',
-                mimetype = 'application/json',
-                status = 500
+            return MakeResponse(
+                JsonResponse({
+                    'Message': 'Blue integer should be greater or equal to 0 and lesser or equal to 255'
+                }),
+                500
             )
         else:
-            return Response(
+            return MakeResponse(
                 JsonResponse({
                     'Red': Red,
                     'Green': Green,
                     'Blue': Blue
                 }),
-                mimetype = 'application/json',
-                status = 200
+                200
             )
