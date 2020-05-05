ae7aa2e0725f78aba610db8394f59551ee1f3337
Xavier Beurois <xbeurois@djazz-univ.net>
2020-05-06 01:49:32 +0200

    Add validation code for RGB color codes

--- Resources/CubeSolver/CubeSolverRgbToLab.py
+++ Resources/CubeSolver/CubeSolverRgbToLab.py
@@ -1,10 +1,34 @@
 from flask import jsonify as JsonResponse
+from flask import Response
 from flask_restful import Resource as APIResource
 
 class CubeSolverRgbToLab(APIResource):
     def get(self, Red, Green, Blue):
-        return JsonResponse({
-            'Red': Red,
-            'Green': Green,
-            'Blue': Blue
-        })
+        if Red < 0 or Red > 255:
+            return Response(
+                'Red integer should be greater or equal to 0 and lesser or equal to 255'
+                mimetype = 'application/json',
+                status = 500
+            )
+        elif Green < 0 or Green > 255:
+            return Response(
+                'Green integer should be greater or equal to 0 and lesser or equal to 255'
+                mimetype = 'application/json',
+                status = 500
+            )
+        elif Blue < 0 or Blue > 255:
+            return Response(
+                'Blue integer should be greater or equal to 0 and lesser or equal to 255'
+                mimetype = 'application/json',
+                status = 500
+            )
+        else:
+            return Response(
+                JsonResponse({
+                    'Red': Red,
+                    'Green': Green,
+                    'Blue': Blue
+                }),
+                mimetype = 'application/json',
+                status = 200
+            )
