83017065ff33262cdd8ff3ba860a6ada264e39d0
Xavier Beurois <xbeurois@djazz-univ.net>
2020-05-06 01:26:08 +0200

    Fix wrong method get declaration in CubeSolverRgbToLab

--- Resources/CubeSolver/CubeSolverRgbToLab/__init__.py
+++ Resources/CubeSolver/CubeSolverRgbToLab/__init__.py
@@ -2,7 +2,7 @@ from flask import jsonify as JsonResponse
 from flask_restful import Resource as APIResource
 
 class CubeSolverRgbToLab(APIResource):
-    def get(Red, Green, Blue):
+    def get(self, Red, Green, Blue):
         return JsonResponse({
             'hello': 'world'
         })
