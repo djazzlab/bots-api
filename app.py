277da1d5de09e190906fa6e7743a9785ee8b34c7
Xavier Beurois <xbeurois@djazz-univ.net>
2020-05-05 23:53:20 +0200

    Test json with list

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ App = Flask(__name__)
 
 @App.route('/')
 def Hello():
-    return JsonResponse({
-        'hello': 'world'
-    })
+    return JsonResponse([
+        {
+            'hello': 'world'
+        },
+        {
+            'hello2': 'world2'
+        }
+    ])
 
 App.run(host = '0.0.0.0')
