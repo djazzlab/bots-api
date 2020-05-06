f4a9bf0dacd02015a81b1b77d6f36dfd890d0460
Xavier Beurois <xbeurois@HAMMOND.local>
2020-05-06 23:07:20 +0200

    Reset Api declaration

--- Resources/__init__.py
+++ Resources/__init__.py
@@ -4,14 +4,6 @@ from Resources.Router import InitializeRoutes
 
 # Create Flask APP
 App = Flask(__name__)
-API = Api(
-    App,
-    description = 'A simple API for hard operations to be done by EV3 robots',
-    version = '1.0',
-    title = 'Bots API'
-)
-
-ApiNS = API.namespace('bots', description = 'Bots hard operations')
 
 # Init routes
-InitializeRoutes(BotsAPI = API)
+InitializeRoutes(BotsAPI = Api(App))
