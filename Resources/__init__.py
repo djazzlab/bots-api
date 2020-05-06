468a644759e6e4842655215825cd5cb5503af7e2
Xavier Beurois <xbeurois@HAMMOND.local>
2020-05-06 23:01:38 +0200

    Include namespace in the API

--- Resources/__init__.py
+++ Resources/__init__.py
@@ -4,6 +4,14 @@ from Resources.Router import InitializeRoutes
 
 # Create Flask APP
 App = Flask(__name__)
+API = Api(
+    App,
+    description = 'A simple API for hard operations to be done by EV3 robots',
+    version = '1.0',
+    title = 'Bots API'
+)
+
+ApiNS = API.namespace('bots', description = 'Bots hard operations')
 
 # Init routes
-InitializeRoutes(BotsAPI = Api(App))
+InitializeRoutes(BotsAPI = API)
