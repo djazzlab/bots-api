1ac361e24113b027d7c669603c5f306cc3605e19
Xavier Beurois <xbeurois@HAMMOND.local>
2020-05-07 00:26:50 +0200

    Implement labs distance calculation

--- Resources/CubeSolver/CubeSolverLabsDistance.py
+++ Resources/CubeSolver/CubeSolverLabsDistance.py
@@ -15,23 +15,95 @@ class CubeSolverLabsDistance(APIResource):
         # Parse arguments in the request body
         self.__ArgsParser = ReqParser.RequestParser()
         self.__ArgsParser.add_argument(
-            'lab1',
-            help = 'First LAB color to calculate the distance',
-            required = True,
-            type = dict
+            'lab1_lightness',
+            location = 'form',
+            store_missing = False,
+            type = float
         )
         self.__ArgsParser.add_argument(
-            'lab2',
-            help = 'Second LAB color to calculate the distance',
-            required = True,
-            type = dict
+            'lab1_dim_a',
+            location = 'form',
+            store_missing = False,
+            type = float
+        )
+        self.__ArgsParser.add_argument(
+            'lab1_dim_b',
+            location = 'form',
+            store_missing = False,
+            type = float
+        )
+        self.__ArgsParser.add_argument(
+            'lab2_lightness',
+            location = 'form',
+            store_missing = False,
+            type = float
+        )
+        self.__ArgsParser.add_argument(
+            'lab2_dim_a',
+            location = 'form',
+            store_missing = False,
+            type = float
+        )
+        self.__ArgsParser.add_argument(
+            'lab2_dim_b',
+            location = 'form',
+            store_missing = False,
+            type = float
         )
 
     def get(self):
         try:
-            Args = self.__ArgsParser.parse_args()
-            print(Args)
+            Args = self.__ArgsParser.parse_args(strict = True)
+
+            LAB1 = tuple()
+            LAB2 = tuple()
+
+            if 'lab1_lightness' in Args:
+                LAB1 += (Args['lab1_lightness'],)
+            else:
+                raise Exception('Your request body must have a value for the lab1_lightness argument')
+
+            if 'lab1_dim_a' in Args:
+                LAB1 += (Args['lab1_dim_a'],)
+            else:
+                raise Exception('Your request body must have a value for the lab1_dim_a argument')
+            
+            if 'lab1_dim_b' in Args:
+                LAB1 += (Args['lab1_dim_b'],)
+            else:
+                raise Exception('Your request body must have a value for the lab1_dim_b argument')
+
+            if 'lab2_lightness' in Args:
+                LAB2 += (Args['lab2_lightness'],)
+            else:
+                raise Exception('Your request body must have a value for the lab2_lightness argument')
+
+            if 'lab2_dim_a' in Args:
+                LAB2 += (Args['lab2_dim_a'],)
+            else:
+                raise Exception('Your request body must have a value for the lab2_dim_a argument')
+            
+            if 'lab2_dim_b' in Args:
+                LAB2 += (Args['lab2_dim_b'],)
+            else:
+                raise Exception('Your request body must have a value for the lab2_dim_b argument')
+
+            self.__LABColor1 = LabColor(
+                lab_l = float(LAB1[0]),
+                lab_a = float(LAB1[1]),
+                lab_b = float(LAB1[2])
+            )
+
+            self.__LABColor2 = LabColor(
+                lab_l = float(LAB2[0]),
+                lab_a = float(LAB2[1]),
+                lab_b = float(LAB2[2])
+            )
+
+            return {
+                'Distance': DeltaCie2000(self.__LABColor1, self.__LABColor2)
+            }
         except Exception as E:
             return {
-                'Message': 'Exception while converting rgb color to lab: {}'.format(E)
+                'Message': str(E)
             }, 500
