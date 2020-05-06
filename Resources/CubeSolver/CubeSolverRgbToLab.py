153e51860cae27233761a25404f23badc825f027
Xavier Beurois <xbeurois@HAMMOND.local>
2020-05-06 14:56:03 +0200

    Remove the RGB codes from the URL

--- Resources/CubeSolver/CubeSolverRgbToLab.py
+++ Resources/CubeSolver/CubeSolverRgbToLab.py
@@ -1,14 +1,26 @@
 # API Imports
 from flask import jsonify as JsonResponse
 from flask import make_response as MakeResponse
+from flask_restful import reqparse as ReqParser
 from flask_restful import Resource as APIResource
 
 # Colormath
 from colormath.color_conversions import convert_color as ConvertColor
 from colormath.color_objects import sRGBColor, LabColor
 
+# Parse arguments in the request body
+ArgsParser = ReqParser.RequestParser()
+ArgsParser.add_argument('red')
+ArgsParser.add_argument('green')
+ArgsParser.add_argument('blue')
+
 class CubeSolverRgbToLab(APIResource):
-    def get(self, Red, Green, Blue):
+    def get(self):
+        Args = ArgsParser.parse_args()
+        Red = Args['red']
+        Green = Args['green']
+        Blue = Args['blue']
+
         if Red < 0 or Red > 255:
             return MakeResponse(
                 JsonResponse({
--- Resources/Router.py
+++ Resources/Router.py
@@ -3,4 +3,4 @@ from Resources.CubeSolver.CubeSolverRgbToLab import CubeSolverRgbToLab
 
 def InitializeRoutes(BotsAPI):
     # CubeSolver
-    BotsAPI.add_resource(CubeSolverRgbToLab, '/bots-api/cube-solver/rgb-to-lab/<int:Red>/<int:Green>/<int:Blue>', endpoint = 'rgb_to_lab')
+    BotsAPI.add_resource(CubeSolverRgbToLab, '/bots-api/cube-solver/rgb-to-lab', endpoint = 'rgb_to_lab')
