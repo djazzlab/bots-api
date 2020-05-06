8e25bad2a239f7d2605c9002431ce00f472f3a41
Xavier Beurois <xbeurois@HAMMOND.local>
2020-05-06 15:33:05 +0200

    Use flask restful builtin return mechanism

--- Resources/CubeSolver/CubeSolverRgbToLab.py
+++ Resources/CubeSolver/CubeSolverRgbToLab.py
@@ -23,26 +23,17 @@ class CubeSolverRgbToLab(APIResource):
             Blue = Args['blue']
 
             if Red < 0 or Red > 255:
-                return MakeResponse(
-                    JsonResponse({
-                        'Message': 'Red integer should be greater or equal to 0 and lesser or equal to 255'
-                    }),
-                    500
-                )
+                return {
+                    'Message': 'Red integer should be greater or equal to 0 and lesser or equal to 255'
+                }, 500
             elif Green < 0 or Green > 255:
-                return MakeResponse(
-                    JsonResponse({
-                        'Message': 'Green integer should be greater or equal to 0 and lesser or equal to 255'
-                    }),
-                    500
-                )
+                return {
+                    'Message': 'Green integer should be greater or equal to 0 and lesser or equal to 255'
+                }, 500
             elif Blue < 0 or Blue > 255:
-                return MakeResponse(
-                    JsonResponse({
-                        'Message': 'Blue integer should be greater or equal to 0 and lesser or equal to 255'
-                    }),
-                    500
-                )
+                return {
+                    'Message': 'Blue integer should be greater or equal to 0 and lesser or equal to 255'
+                }, 500
             else:
                 Lab = ConvertColor(
                     sRGBColor(Red, Green, Blue, True),
@@ -50,20 +41,14 @@ class CubeSolverRgbToLab(APIResource):
                 )
                 LabTuple = Lab.get_value_tuple()
 
-                return MakeResponse(
-                    JsonResponse({
-                        'Lab': {
-                            'L': LabTuple[0],
-                            'A': LabTuple[1],
-                            'B': LabTuple[2]
-                        }
-                    }),
-                    200
-                )
+                return {
+                    'Lab': {
+                        'L': LabTuple[0],
+                        'A': LabTuple[1],
+                        'B': LabTuple[2]
+                    }
+                }
         except Exception as E:
-            return MakeResponse(
-                JsonResponse({
-                    'Message': 'Exception while converting rgb color to lab: {}'.format(E)
-                }),
-                500
-            )
+            return {
+                'Message': 'Exception while converting rgb color to lab: {}'.format(E)
+            }, 500
